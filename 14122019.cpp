96e914d39201dda10f0b2fc84453a9cd1408ce37
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2020-07-19 17:45:53 +0300

    Добавьте файлы проекта.

---
+++ 14122019.cpp
@@ -0,0 +1,66 @@
+﻿// 14122019.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	std::cout << "Привіт Житомир\n" << endl;
+
+	std::cout << "**********************\n";
+	std::cout << "***Віктор Блажкевич***\n";
+	std::cout << "**********************";
+	std::cout << "\n\n";
+
+	SetConsoleOutputCP(866);
+
+	std::cout << "**********************\n";
+	std::cout << "**Viktor Blazhkevych**\n";
+	std::cout << "**********************";
+	std::cout << "\n\n";
+
+	std::cout << (char)201 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)187 << endl;
+	std::cout << (char)186 << "**Viktor Blazhkevych**" << (char)186 << endl;
+	std::cout << (char)200 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)188 << endl;
+
+	SetConsoleOutputCP(866);
+	std::cout << (char)201 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)187 << endl;
+	std::cout << (char)186;
+	SetConsoleOutputCP(1251);
+	std::cout << "***Віктор Блажкевич***";
+	SetConsoleOutputCP(866);
+	std::cout << (char)186 << endl;
+	std::cout << (char)200 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)188 << endl;
+
+
+
+
+	std::cout << "\n\nD:\\new\\temp.\\bmp.txt";
+
+	std::cout << "\n\nHello \"ITStep\"";
+
+
+
+
+
+	std::cout << "\n\n";
+
+
+
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
